d6426938728a9a07b80350c192439eb010032b4d
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-05-07 18:39:58 +0700

    Hết tiết, bye

--- millionaire.sh
+++ millionaire.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
-echo "helo world"
-sefslrf
-123
+clear
+echo "Chào mừng đến với chương trình Đi tìm triệu phú!"
+
+soCau=0
+while read line; do
+	soCau=$(($soCau+1))
+done < cauhoi.txt
+echo "Số câu: "$soCau
+
+continue=1
+while [ $continue -eq 1 ]; do
+	current=$(( ($RANDOM%$soCau) +1))
+	echo "Câu hiện tại: "$current
+	# Xuất câu hỏi từ cauhoi.txt
+	cauhoi=`cat cauhoi.txt | head -$current | tail -1`
+	#
+	# Lưu tiến trình: Câu hỏi đã chọn
+
+	echo $cauhoi
+	read
+done
