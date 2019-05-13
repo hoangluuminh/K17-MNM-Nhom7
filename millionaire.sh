fff5f83b86f04685271b6f1203c2e4cc9125fdd1
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-05-13 13:20:04 +0700

    Sửa lỗi

--- millionaire.sh
+++ millionaire.sh
@@ -7,40 +7,41 @@ echo "Số câu: "$soCau
 
 # Tạo file tiến trình
 rm progress.txt &> /dev/null
-for ((i=1; i<=$soCau; i++)); do
+for ((i=0; i<$soCau; i++)); do
 	echo "0|0" >> progress.txt
 done
 
 # TrangThai: 0: Chưa chọn; 1: Đã bỏ qua; 2: Đã chọn
 # ChonDung: 0: Sai; 1: Đúng
-trangThai=()
-chonDung=()
 
 continue=1
 while [ $continue -eq 1 ]; do
-	# Lấy câu hỏi chưa chọn
-	current=$(( ($RANDOM%$soCau) +1))
-	while [ `cat progress.txt | head -$current | tail -1 | cut -d'|' -f1` -ne 0 ]; do
-		current=$(( ($RANDOM%$soCau) +1))
-	done
-	
 	# Đọc dữ liệu tiến trình của người chơi hiện tại
+	trangThai=()
+	chonDung=()
+	cat progress.txt
 	while read line; do
 		trangThai+=(`echo $line | cut -d'|' -f1`)
 		chonDung+=(`echo $line | cut -d'|' -f2`)
 	done < progress.txt
 	rm progress.txt &> /dev/null
+	
+	# Lấy câu hỏi chưa chọn
+	current=$(( ($RANDOM%$soCau) ))
+	while [ ${trangThai[$current]} -ne 0 ]; do
+		current=$(( ($RANDOM%$soCau) ))
+	done
 	echo "Câu hiện tại: "$current
 
 	# Xuất câu hỏi từ cauhoi.txt
-	cauhoi=`cat cauhoi.txt | head -$current | tail -1 | cut -d'|' -f1`
+	cauhoi=`cat cauhoi.txt | head -$(($current+1)) | tail -1 | cut -d'|' -f1`
 	echo "Câu hỏi: $cauhoi"
 
 	# Hiển thị câu trả lời
-	cauA=`cat cauhoi.txt | head -$current | tail -1 | cut -d'|' -f3`
-	cauB=`cat cauhoi.txt | head -$current | tail -1 | cut -d'|' -f4`
-	cauC=`cat cauhoi.txt | head -$current | tail -1 | cut -d'|' -f5`
-	cauD=`cat cauhoi.txt | head -$current | tail -1 | cut -d'|' -f6`
+	cauA=`cat cauhoi.txt | head -$(($current+1)) | tail -1 | cut -d'|' -f3`
+	cauB=`cat cauhoi.txt | head -$(($current+1)) | tail -1 | cut -d'|' -f4`
+	cauC=`cat cauhoi.txt | head -$(($current+1)) | tail -1 | cut -d'|' -f5`
+	cauD=`cat cauhoi.txt | head -$(($current+1)) | tail -1 | cut -d'|' -f6`
 	echo "A: $cauA"
 	echo "B: $cauB"
 	echo "C: $cauC"
@@ -63,7 +64,7 @@ while [ $continue -eq 1 ]; do
 	
 	# Lưu tiến trình: Câu hỏi đã chọn
 	trangThai[$current]=2
-	for ((i=1; i<=$soCau; i++)); do
+	for ((i=0; i<$soCau; i++)); do
 		echo ${trangThai[$i]}"|"${chonDung[$i]} >> progress.txt 
 	done
 	
